e83cdd04123bd5a86dfda0b4acd6e3e861caf51f
Walter Lee Davis <waltd@wdstudio.com>
2025-01-30 11:03:54 -0500

    more refactor to order

--- app/models/custom_product.rb
+++ app/models/custom_product.rb
@@ -1,11 +1,10 @@
 class CustomProduct < ApplicationRecord
-  belongs_to :person
+  belongs_to :order
   belongs_to :color
   belongs_to :shape
 
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
-  accepts_nested_attributes_for :person, reject_if: :all_blank
 
   def description
     "#{quantity} of #{shape.name} in #{color.name}"
