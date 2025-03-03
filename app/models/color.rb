fe6d897622d1db82fe558b6816d8e2611e4a6743
Walter Lee Davis <waltd@wdstudio.com>
2025-03-02 19:55:22 -0500

    add id and has many

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,5 +1,8 @@
 class Color < ApplicationRecord
   validates :name, presence: true
   acts_as_list top_of_list: 0
+
+  has_many :custom_products
+
   acts_as_paranoid without_default_scope: true
 end
--- app/models/shape.rb
+++ app/models/shape.rb
@@ -1,6 +1,8 @@
 class Shape < ApplicationRecord
   validates :name, presence: true
   acts_as_list top_of_list: 0
+  
+  has_many :custom_products
 
   monetize :price_cents
   acts_as_paranoid without_default_scope: true
